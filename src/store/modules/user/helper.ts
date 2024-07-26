3fdbeb8eec20344ec75f8d8203ffb52b18e87318
bcat95 <bcat9505@gmail.com>
2024-07-26 16:28:43 +0700

    v1.0.0

---
+++ src/store/modules/user/helper.ts
@@ -0,0 +1,32 @@
+import { ss } from '@/utils/storage'
+
+const LOCAL_NAME = 'userStorage'
+
+export interface UserInfo {
+  avatar: string
+  name: string
+  description: string
+}
+
+export interface UserState {
+  userInfo: UserInfo
+}
+
+export function defaultSetting(): UserState {
+  return {
+    userInfo: {
+      avatar: 'https://chatvn.org/assets/favicon.png',
+      name: 'chatvn.org',
+      description: 'Trải nghiệm ngay <a href="https://chatvn.org" class="text-blue-500" target="_blank" >Chat GPT không cần tài khoản</a>',
+    },
+  }
+}
+
+export function getLocalState(): UserState {
+  const localSetting: UserState | undefined = ss.get(LOCAL_NAME)
+  return { ...defaultSetting(), ...localSetting }
+}
+
+export function setLocalState(setting: UserState): void {
+  ss.set(LOCAL_NAME, setting)
+}
